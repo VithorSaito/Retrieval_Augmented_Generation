d11247070014bfa86cc4f7c641fbab210fd553d9
Vithor <devVithor@gmail.com>
2025-11-27 16:03:56 -0300

    chore: update names

--- src/routes/called.routes.ts
+++ src/routes/called.routes.ts
@@ -1,13 +1,13 @@
 import { FastifyInstance } from "fastify";
 
 import { createQuestionController } from "../controller/questionController/create";
-import { createKnowledgeBaseController } from "../controller/knowledgeBaseController/create";
+import { createKnowledgeController } from "../controller/knowledgeBaseController/create";
 
 export const calledRoutes = async (server: FastifyInstance) => {
 
   server
-    .post("/knowledgeBase", async (request, reply) => {
-      const result = await createKnowledgeBaseController.execute(request, reply)
+    .post("/knowledge", async (request, reply) => {
+      const result = await createKnowledgeController.execute(request, reply)
 
       return result
 
